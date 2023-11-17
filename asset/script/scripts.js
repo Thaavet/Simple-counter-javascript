8d2b221480ebd1ce407c226bc0ee4b3e937cafc3
Thaavet <148262279+Thaavet@users.noreply.github.com>
2023-11-17 21:45:05 +0100

    No HTML, Only Js

--- asset/script/scripts.js
+++ asset/script/scripts.js
@@ -1,9 +1,47 @@
-const total = document.querySelector('#count');
+
+var container = document.createElement('div');
+container.className = 'container';
+
+var counterBody = document.createElement('div');
+counterBody.className = 'counter-body';
+
+var count = document.createElement('h1');
+count.id = 'count';
+count.textContent = '0';
+
+var counterButton = document.createElement('div');
+counterButton.className = 'counter-button';
+
+var minusButton = document.createElement('button');
+minusButton.id = 'minus';
+minusButton.textContent = '-';
+
+var resetButton = document.createElement('button');
+resetButton.id = 'reset';
+resetButton.textContent = 'Reset';
+
+var plusButton = document.createElement('button');
+plusButton.id = 'plus';
+plusButton.textContent = '+';
+
+counterButton.appendChild(minusButton);
+counterButton.appendChild(resetButton);
+counterButton.appendChild(plusButton);
+
+counterBody.appendChild(count);
+counterBody.appendChild(counterButton);
+
+container.appendChild(counterBody);
+
+document.body.appendChild(container);
+
+
+const total = document.getElementById('count');
 let value = 0;
 
-const btnDecrement = document.querySelector('#minus');
-const btnReset = document.querySelector('#reset');
-const btnIncrement = document.querySelector('#plus');
+const btnIncrement = document.getElementById('plus');
+const btnDecrement = document.getElementById('minus');
+const btnReset = document.getElementById('reset');
 
 btnIncrement.addEventListener('click', () => {
   value++;
